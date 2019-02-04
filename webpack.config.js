1da1bf026e9e044d9c88c6d478b3eb2ac01cbe1f
Alex Warren <alex@alexwarren.uk>
2019-02-04 21:07:29 +0000

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
 var path = require('path');
 
 module.exports = {
+    mode: 'development',
     watch: true,
     devtool: 'source-map',
     entry: {
         app: ['./quest.js']
     },
-    path: path.resolve(__dirname, 'dist'),
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
     output: {
         filename: 'quest.js'
-    },
-    module: {
-        preLoaders: [{
-            test: /asl4\.js$/,
-            loader: 'source-map-loader'
-        }]
     }
 };
