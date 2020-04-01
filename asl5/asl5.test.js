37cb53b7b3f5e42d61ed03b986843cbb760b7926
Alex Warren <alex@alexwarren.uk>
2020-04-01 11:12:58 +0100

    Adding test for asl5.js

---
+++ asl5/asl5.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const asl5 = require('./asl5');
+const fs = require('fs');
+
+window.addTextAndScroll = (text) => {
+    console.log(text);
+};
+
+window.uiShow = () => {};
+window.uiHide = () => {};
+
+test('just a test', () => {
+    const data = fs.readFileSync('examples/test.aslx', 'utf-8');
+    asl5.load(data);
+    asl5.begin();
+    
+    // TODO: Capture output and compare against a snapshot
+});
