275a9b5dcc34c502ffcb033a1e99cda415ce8baa
Alex Warren <alex@alexwarren.uk>
2020-04-03 18:26:28 +0100

    Build ASL4 with inline source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');  
-const sourcemaps = require('gulp-sourcemaps');  
-const ts = require('gulp-typescript');  
-const babel = require('gulp-babel');
 const webpack = require('webpack-stream');
-
-gulp.task('asl4', () => {
-    const tsProject = ts.createProject('asl4/tsconfig.json');
-      
-    return gulp.src('asl4/asl4.ts')
-        .pipe(sourcemaps.init())
-        .pipe(tsProject())
-        .pipe(babel())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./asl4'));
-});
 
 gulp.task('webpack', () => {
     return gulp.src('quest.js')
